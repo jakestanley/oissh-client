f733f51b852d895e75469ae7815d7fc2ab2bcee3
Jacob Stanley <prudentia713@gmail.com>
2021-12-30 00:28:49 +0000

    rudimentary networking

--- main.go
+++ main.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
+	"net"
 	"strings"
 
 	ui "github.com/gizak/termui/v3"
 	"golang.org/x/crypto/ssh"
 )
 
+var (
+	hasSession bool = false
+	client     *ssh.Client
+	session    *ssh.Session
+	stdin      io.WriteCloser
+)
+
 func main() {
+
+	// for vscode debugging purposes
+	// connect("localhost:2223")
 
 	initUi()
 	defer ui.Close()
@@ -18,30 +30,105 @@ func main() {
 
 	for inputUi() {
 	}
+
+	if hasSession {
+		defer session.Close()
+		defer client.Close()
+	}
+
+	// TODO fix. segfaults
+	// err := session.Close()
+	// if err != nil {
+	// 	log.Println("Could not close session. Probably not open or already closed")
+	// } else {
+	// 	log.Println("SSH session ended gracefully")
+	// }
 }
 
-func getPublicKey() string {
-	publicKeyPath := "/Users/jake/.ssh/oissh.pub"
+func getPublicKey() ssh.AuthMethod {
+	// publicKeyPath := "/Users/jake/.ssh/oissh.pub"
 	privateKeyPath := "/Users/jake/.ssh/oissh"
 
-	buffer, err := ioutil.ReadFile(publicKeyPath)
+	buffer, err := ioutil.ReadFile(privateKeyPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	key, err = ssh.ParsePrivateKey(buffer)
+	key, err := ssh.ParsePrivateKey(buffer)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	return ssh.PublicKeys(key), nil
+	return ssh.PublicKeys(key)
+}
+
+func cmd(cmd string) {
+
+	fmt.Printf("Sending: '%s'", cmd)
+	fmt.Fprintf(stdin, "%s\n", cmd)
+
+	// if err := session.Start(cmd); err != nil {
+	// 	log.Println("session.Run error")
+	// 	log.Fatal(err)
+	// }
 }
 
 func connect(cxn string) {
 
-	ppk := getPublicKey()
-
 	fmt.Printf("Connecting to %s\n", cxn)
+	ppk := getPublicKey()
+	user := "jake"
+
+	verificationCallback := func(hostname string, remote net.Addr, key ssh.PublicKey) error {
+		// TODO implement
+		return ssh.InsecureIgnoreHostKey()(hostname, remote, key)
+		// this will be what confirms the server identity,
+		// 	like that MitM message you get when a server
+		// 	you're connecting to via CLI has its fingerprint changed
+	}
+
+	config := &ssh.ClientConfig{
+		User: user,
+		Auth: []ssh.AuthMethod{
+			ppk,
+		},
+		HostKeyCallback: verificationCallback,
+	}
+
+	var err error
+	client, err = ssh.Dial("tcp", cxn, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	session, err = client.NewSession()
+	if err != nil {
+		log.Fatal(err)
+	}
+	hasSession = true
+
+	// modes := ssh.TerminalModes{
+	// 	ssh.ECHO:  0,
+	// 	ssh.IGNCR: 1,
+	// }
+
+	// if err = session.RequestPty("vt100", 80, 40, modes); err != nil {
+	// 	log.Println("session.RequestPty error")
+	// 	log.Fatal(err)
+	// }
+
+	stdin, err = session.StdinPipe()
+	if err != nil {
+		log.Println("session.StdinPipe error")
+		log.Fatal(err)
+	}
+
+	if err = session.Shell(); err != nil {
+		log.Println("session.Shell error")
+		log.Fatal(err)
+	}
+
+	log.Println("Connected!")
 }
 
 func processInput(input string) {
@@ -53,8 +140,16 @@ func processInput(input string) {
 
 	switch strings.ToLower(commands[0]) {
 	case "c", "connect":
-		connect(commands[1])
+		if commands[1] == "default" {
+			go connect("localhost:2223")
+		} else {
+			go connect(commands[1])
+		}
+
+	// case "hello":
+	//
 	default:
+		cmd(input)
 		return
 	}
 }
